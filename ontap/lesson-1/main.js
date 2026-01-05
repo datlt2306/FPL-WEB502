c7bb92661d32c637e938db2f47ab7d0b040bbfe0
datlt2306 <datlt2306@gmail.com>
2026-01-05 17:18:35 +0700

    fix: cải thiện logic hiển thị thông báo
    - Điều chỉnh điều kiện kiểm tra để thông báo hiển thị chính xác hơn.
    - Sửa lỗi không cập nhật trạng thái sau khi thực hiện hành động.

---
+++ ontap/lesson-1/main.js
@@ -0,0 +1,8 @@
+var a = 10;
+var b = 20;
+console.log(a + b);
+// thông tin
+var myName = "Lê Trọng Đạt";
+var myAge = 38;
+var myStatus = true;
+console.log(myName, myAge, myStatus);
---
+++ ontap/lesson-1/main.ts
@@ -0,0 +1,14 @@
+const a: number = 10;
+const b: number = 20;
+console.log(a + b);
+// thông tin
+const myName: string = "Lê Trọng Đạt";
+const myAge: number = 38;
+const myStatus: boolean = true;
+const myChildren = ['Kiên', 'Trung'];
+const myProfile = {
+    name: 'Lê Trọng Đạt',
+    age: 38,
+    address: "Thái Bình"
+}
+console.log(myName, myAge, myStatus);
