839d5f9fab14d84c858e4982295a6d7966bf86c9
datlt2306 <datlt2306@gmail.com>
2026-01-07 13:00:55 +0700

    chore: update script source path in index.html
    - Changed the script source from './main.js' to './dist/main.js' and set type to module for improved module loading.

---
+++ ontap/lesson-2/dist/main.js
@@ -0,0 +1,9 @@
+const myChildren = ['Kiên', 'Trung'];
+const myProfile = {
+    name: 'Lê Trọng Đạt',
+    age: 39,
+    address: "Thái Bình",
+};
+console.log(myChildren, myProfile);
+export {};
+//# sourceMappingURL=main.js.map
