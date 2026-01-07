d284669860af1293f9245985a538a4600825e7c3
datlt2306 <datlt2306@gmail.com>
2026-01-07 12:58:17 +0700

    refactor: update variable types and modify profile age
    - Changed myChildren to explicitly define its type as string array.
    - Updated myProfile to include type annotations for better clarity.
    - Modified age in myProfile from 38 to 39.
    - Added comments for TypeScript installation instructions.

--- ontap/lesson-1/main.ts
+++ ontap/lesson-1/main.ts
@@ -5,10 +5,19 @@ console.log(a + b);
 const myName: string = "Lê Trọng Đạt";
 const myAge: number = 38;
 const myStatus: boolean = true;
-const myChildren = ['Kiên', 'Trung'];
-const myProfile = {
+const myChildren: string[] = ['Kiên', 'Trung'];
+
+const myProfile: {
+    name: string;
+    age: number;
+    address: string
+} = {
     name: 'Lê Trọng Đạt',
-    age: 38,
-    address: "Thái Bình"
+    age: 39,
+    address: "Thái Bình",
 }
 console.log(myName, myAge, myStatus);
+
+// Cài đặt nodejs
+// npm install -g typescript
+// 
---
+++ ontap/lesson-2/main.ts
@@ -0,0 +1,11 @@
+const myChildren: string[] = ['Kiên', 'Trung'];
+const myProfile: {
+    name: string;
+    age: number;
+    address: string
+} = {
+    name: 'Lê Trọng Đạt',
+    age: 39,
+    address: "Thái Bình",
+}
+console.log(myChildren, myProfile);
